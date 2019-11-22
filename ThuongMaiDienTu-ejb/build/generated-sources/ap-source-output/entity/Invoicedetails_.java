0653544955fe3d19a7b3c7bbc49e839dd263e90b
jamenco3 <jamenco3@gmail.com>
2019-11-22 17:31:37 +0700

    commit

--- ThuongMaiDienTu-ejb/build/generated-sources/ap-source-output/entity/Invoicedetails_.java
+++ ThuongMaiDienTu-ejb/build/generated-sources/ap-source-output/entity/Invoicedetails_.java
@@ -7,7 +7,11 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
+<<<<<<< Updated upstream
 @Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-22T17:09:27")
+=======
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-22T17:27:32")
+>>>>>>> Stashed changes
 @StaticMetamodel(Invoicedetails.class)
 public class Invoicedetails_ { 
 
