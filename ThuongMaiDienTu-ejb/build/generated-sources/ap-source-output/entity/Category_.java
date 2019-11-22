62bbeac1641ec5dd04bf9e8609358cfdf2866191
thainguyen1912 <thainguyen24111998@gmail.com>
2019-11-22 17:54:51 +0700

    a

--- ThuongMaiDienTu-ejb/build/generated-sources/ap-source-output/entity/Category_.java
+++ ThuongMaiDienTu-ejb/build/generated-sources/ap-source-output/entity/Category_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-22T17:53:01")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-22T17:54:37")
 @StaticMetamodel(Category.class)
 public class Category_ { 
 
