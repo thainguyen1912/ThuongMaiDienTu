c0f24a43f9253d94ad6fba0ab782bc9c900be09d
thainguyen1912 <thainguyen24111998@gmail.com>
2019-11-30 16:58:07 +0700

    a

--- ThuongMaiDienTu-ejb/build/generated-sources/ap-source-output/entity/Customer_.java
+++ ThuongMaiDienTu-ejb/build/generated-sources/ap-source-output/entity/Customer_.java
@@ -7,11 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< Updated upstream
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-22T22:49:27")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-22T22:52:26")
->>>>>>> Stashed changes
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-22T22:55:53")
 @StaticMetamodel(Customer.class)
 public class Customer_ { 
 
